2e5e92fed19da34392fbf9fdbe4a4cbabfa827b2
Joachim Prinzbach <joachim.prinzbach@baloise.ch>
2024-06-23 13:20:34 +0200

    add login and logout buttons

--- src/main/java/com/baloise/platformplanedemoapp/ThymeleafIndexController.java
+++ src/main/java/com/baloise/platformplanedemoapp/ThymeleafIndexController.java
@@ -1,11 +1,14 @@
 package com.baloise.platformplanedemoapp;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
 
 @Controller
 public class ThymeleafIndexController {
@@ -15,7 +18,13 @@ public class ThymeleafIndexController {
     SecurityContext context = SecurityContextHolder.getContext();
     if(context.getAuthentication().getPrincipal() instanceof DefaultOidcUser user) {
       String userName = user.getName();
-      model.addAttribute("currentUser", userName);
+      String mail = user.getPreferredUsername();
+      model.addAttribute("currentUser", userName + " (" + mail + ")");
+      Collection<? extends GrantedAuthority> authorities = user.getAuthorities();
+      model.addAttribute("role", authorities.iterator().next());
+      model.addAttribute("issuer", "Azure EntraID");
+      model.addAttribute("mail", mail);
+      model.addAttribute("name", userName);
     } else {
       model.addAttribute("currentUser", "Anonymous");
     }
