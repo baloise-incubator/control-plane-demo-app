7ebdc3bddf4645f4b5e1997ee8d4382a3f02d592
Joachim Prinzbach <joachim.prinzbach@baloise.ch>
2024-07-02 23:12:44 +0200

    Add headers endpoint

--- src/main/java/com/baloise/platformplanedemoapp/todo/TodoRestController.java
+++ src/main/java/com/baloise/platformplanedemoapp/todo/TodoRestController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -35,5 +37,13 @@ public class TodoRestController {
   @DeleteMapping("/{id}")
   public void delete(@PathVariable Long id) {
     todoRepository.deleteById(id);
+  }
+
+  @GetMapping("/headers")
+  public String getHeaders(@RequestHeader Map<String, String> headers) {
+    headers.forEach((key, value) -> {
+      System.out.printf("Header '%s' = %s%n", key, value);
+    });
+    return "success";
   }
 }
